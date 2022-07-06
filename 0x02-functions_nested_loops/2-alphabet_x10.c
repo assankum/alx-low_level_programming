1f78faec63fe25abe6cb18adbae1ae77602f7730
assankum <61219865+assankum@users.noreply.github.com>
2022-07-06 17:43:18 +0000

    Update 2-alphabet_x10.c

--- 0x02-functions_nested_loops/2-alphabet_x10.c
+++ 0x02-functions_nested_loops/2-alphabet_x10.c
@@ -8,13 +8,13 @@
 
 void print_alphabet_x10(void)
 {
-	int count = 0;
-	char letter;
+	int i;
+	char c;
 
-	while (count++ <= 9)
+	for (i = 0; i <= 'z'; i++)
 	{
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
+		for (c = 'a'; a <= 'z'; a++)
+			_putchar(a);
 		_putchar('\n');
 	}
 }
