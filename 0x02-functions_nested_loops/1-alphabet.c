788068d39dbf0f262e57cf71d77e85660a9c5e04
assankum <61219865+assankum@users.noreply.github.com>
2022-07-06 11:58:11 +0000

    Create 1-alphabet.c
    added body of print_alphabet function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_alpahabet - prints the alphabet.
+*
+* Description: prints the alphabet, in lowercase, followed by a new line.
+* Return: 0 (success)
+*/
+
+void print_alphabet(void)
+{
+  
+  char alph;
+  
+  for (alpha = 'a'; alpha <= 'z', alpha++)
+    _putchar(alpha);
+  _putchar('\n');
+}
