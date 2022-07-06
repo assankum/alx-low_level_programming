4accb6677b2e77ef3e49e82aed286f5f18bd595b
assankum <61219865+assankum@users.noreply.github.com>
2022-07-06 17:18:31 +0000

    Create 2-alphabet_x10.c
    added description for function that prints alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - prints alphabets 10 times
+*
+* Description: prints  alphabet, in lowercase, 10 times followed by a new line.
+*/
+
+void print_alphabet_x10(void)
+{
+  
+    for(int i = 0; i < 10; i++)
+    {
+      
+        for(char c = 'a'; c <= 'z'; c++)
+           _putchar(c);
+    }
+  _putchar('\n');
+}
