ea9eabe5fb48f6fc864ffd70e4fa811a229b1949
assankum <61219865+assankum@users.noreply.github.com>
2022-07-06 11:18:07 +0000

    Create main.h + add declaration of _putchar

---
+++ 0x02-finctions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+* File: main.h
+* Auth: Assankum
+* Desc: Header file containing declaration of functions
+        used in the 0x02-functions_nested_loops directory
+*/
+
+int _putchar(char c);
+
+#endif
