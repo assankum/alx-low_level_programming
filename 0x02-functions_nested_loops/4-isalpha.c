25f9e3aec187e89b77726a593ba3d2e455e32ee0
assankum <61219865+assankum@users.noreply.github.com>
2022-07-07 13:07:20 +0000

    Create 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* _isalpha - checks if character is an alphabet
+* @c: character to check
+*
+* Return: 1 for success and 0 for failure
+*/
+
+int _isalpha(int c)
+{
+  int i = 0;
