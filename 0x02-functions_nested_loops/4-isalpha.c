735689e6bb3d5b86b737f4737574c282d4ed9d93
assankum <61219865+assankum@users.noreply.github.com>
2022-07-07 14:43:46 +0000

    Added files 4 to 104

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,16 +1,21 @@
-#include "main.h"
+/*
+ * File: 4-isalpha.c
+ * Auth: Brennan D Baraban
+ */
+
+#include "holberton.h"
 
 /**
-* _isalpha - checks if character is an alphabet
-* @c: character to check
-*
-* Return: 1 for success and 0 for failure
-*/
-
+ * _isalpha - Checks if a character is alphabetic.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if character is letter, lowercase or uppercase, 0 otherwise.
+ */
 int _isalpha(int c)
 {
-    if((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
-      return (1);
-    else
-      return (0);
+	if ((c >= 'a' && c <= 'z') ||
+	    (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
 }
