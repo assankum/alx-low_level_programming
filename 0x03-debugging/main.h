00a6d974a16b098675eebc847d192d665910f66a
assankum <61219865+assankum@users.noreply.github.com>
2022-07-08 21:40:36 +0000

    Add files via upload

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/**
+ * Desc - defines the prototype of functions
+ *
+ * Return: 0 (success)
+ */
+
+void positive_or_negative(int i);
+
+#endif
+	
