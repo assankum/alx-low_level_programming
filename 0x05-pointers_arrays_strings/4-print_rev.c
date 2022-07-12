da31709a5b095f015a5469fd235d9b00245033c0
assankum <61219865+assankum@users.noreply.github.com>
2022-07-12 05:15:48 +0000

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,18 +12,12 @@
 void print_rev(char *s)
 {
 	int len = 0, index;
-	char tmp;
-
-	while (s[index++])
+	
+	while (*s++)
 		len++;
 
-	for (index = len - 1; index >= len / 2; index--)
-	{
-		tmp = s[index];
-		s[index] = s[len - index - 1];
-		s[len - index - 1] = tmp;
+	for (index = len - 1; index >= 0; index--)
 		_putchar(s[index]);
-	}
 
 	_putchar('\n');
 }
