0ae2f12f219bed8d555a0a2c986f16282fc72e63
assankum <assankum0@gmail.com>
2022-07-11 19:39:02 -0700

    added 3-puts.c and updated README + main.h

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - prints string passed as a param followed by a new line
+ * @str: string to be printed to the std output
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	while (*str++)
+		_putchar(*str);
+
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -4,3 +4,5 @@ File 1-swap writes a c function to swap the values of two integers
 
 File 2-strlen writes a c function that gets the length of the string passed as aparameter
 
+File 3-puts writes a function that prints a string, followed by a new line
+
