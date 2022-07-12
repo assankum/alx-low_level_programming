f2b703f7b9a1c032b4edca58da4e18d97a14a5e3
assankum <assankum0@gmail.com>
2022-07-11 21:42:46 -0700

    added 4-print_rev and updated README + main.h

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,18 +8,13 @@
 
 void print_rev(char *s)
 {
-	char *r_ptr = s;
+	int len = 0, index;
 
-	while (*(r_ptr + 1) != '\0')
-		r_ptr++;
+	while (s[index++])
+		len++;
 
-	while (r_ptr > s)
-	{
-		char *tmp = *r_ptr;
-		*r_ptr-- = *s;
-		*s++ = tmp;
-		_putchar(*s);
-	}
+	for (index = len - 1; index >= 0; index--)
+		_putchar(s[index]);
 
 	_putchar('\n');
 }
