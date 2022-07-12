9bb7255b66e1d85884a466bb1ecb0e4ac410f1c7
assankum <assankum0@gmail.com>
2022-07-12 06:02:55 -0700

    updated strcpy function

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -15,12 +15,14 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int index = 0;
+	int index = 0, len = 0;
 
-	while (src[index])
+	while (src[index++])
+		len++;
+
+	for (index = 0; index <= len; index++)
 	{
 		dest[index] = src[index];
-		index++;
 	}
 
 	return (dest);
