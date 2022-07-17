f067d788083f74bd85d6281571b34d2f4e132467
assankum <assankum0@gmail.com>
2022-07-16 17:06:40 -0700

    added directory 0x06-pointers_arrays_strings with README.md+main.h+0-strcat.c

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* 
+ * file - main.h
+ * desc - contains function protoptypes for files in
+ * 0x06-pointers_arrays_strings directory
+ */
+
+char *_strcat(char *dest, char *src);
+
+#endif
